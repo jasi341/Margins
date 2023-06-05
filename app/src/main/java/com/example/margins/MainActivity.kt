fde36c471e619e649be1ba6b01a815af903dba9b
jasi <jasmee.singh@carnot.co.in>
2023-06-05 11:05:12 +0530

    dummy fonts

--- app/src/main/java/com/example/margins/MainActivity.kt
+++ app/src/main/java/com/example/margins/MainActivity.kt
@@ -3,20 +3,18 @@ package com.example.margins
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.margins.ui.theme.MarginsTheme
@@ -31,17 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = Color.Blue
                 ) {
-                    Column(
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        modifier = Modifier.size(250.dp)
-                    ) {
-                        PreviewViewComposable()
-                        Text(
-                            text = "Scan QR Code",
-                            modifier = Modifier.padding(top = 48.dp)
-                        )
-                    }
+                    Greeting(name = "Android", modifier = Modifier.padding(16.dp))
                 }
             }
         }
@@ -50,27 +38,29 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Column {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+    verticalArrangement = Arrangement.Center,
+        modifier = modifier.fillMaxSize().background(Color(0xFF00FF00))
+    ) {
         Text(
             text = "Hello $name!",
             modifier = modifier,
-            style = getDeviceDimensions(
-                deviceType = DeviceType.MOBILE,
-                screenOrientation = LocalConfiguration.current
-            ).normalTextStyle
+            style = getFontStyle(
+                deviceType = DeviceType.MOBILE
+            )
         )
         Text(
             text = "Hello $name!",
             modifier = modifier,
-            style = getDeviceDimensions(
-                deviceType = DeviceType.MOBILE,
-                screenOrientation = LocalConfiguration.current
-            ).boldTextStyle
+            style = getFontStyle(
+                deviceType = DeviceType.TABLET
+            )
         )
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, device = Devices.WEAR_OS_SMALL_ROUND)
 @Composable
 fun GreetingPreview() {
     MarginsTheme {
--- app/src/main/java/com/example/margins/utils.kt
+++ app/src/main/java/com/example/margins/utils.kt
@@ -1,28 +1,10 @@
 package com.example.margins
 
-import android.annotation.SuppressLint
 import android.content.res.Configuration
-import android.util.Log
-import android.widget.Toast
-import androidx.activity.ComponentActivity
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.ImageAnalysis
-import androidx.camera.core.ImageCapture
-import androidx.camera.core.Preview
-import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.camera.view.PreviewView
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.size
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.viewinterop.AndroidView
-import androidx.core.content.ContextCompat
-import java.util.concurrent.Executors
 
 
 enum class DeviceType{
@@ -35,8 +17,7 @@ data class DeviceDimensions(
     val end: Dp,
     val top: Dp = 0.dp,
     val bottom: Dp = 0.dp,
-    val normalTextStyle: TextStyle,
-    val boldTextStyle : TextStyle
+
 )
 
 fun getDeviceDimensions(
@@ -52,19 +33,7 @@ fun getDeviceDimensions(
                     top = 20.dp,
                     bottom = 10.dp,
 
-                    normalTextStyle = TextStyle(
-                        fontSize = 16.sp,
-                        lineHeight = 24.sp,
-                        letterSpacing = 0.15.sp,
-                        fontFamily = FontFamily.Cursive
-                    ),
 
-                    boldTextStyle = TextStyle(
-                        fontSize = 32.sp,
-                        lineHeight = 24.sp,
-                        letterSpacing = 0.15.sp,
-                        fontFamily = FontFamily.Cursive
-                    ),
                 )
             } else {
                 DeviceDimensions(
@@ -72,19 +41,7 @@ fun getDeviceDimensions(
                     end = 15.dp,
                     top = 20.dp,
                     bottom = 10.dp,
-                    normalTextStyle = TextStyle(
-                        fontSize = 16.sp,
-                        lineHeight = 24.sp,
-                        letterSpacing = 0.15.sp,
-                        fontFamily = FontFamily.SansSerif
-                    ),
 
-                    boldTextStyle = TextStyle(
-                        fontSize = 32.sp,
-                        lineHeight = 32.sp,
-                        letterSpacing = 0.15.sp,
-                        fontFamily = FontFamily.Cursive
-                    ),
                 )
             }
         }
@@ -96,19 +53,6 @@ fun getDeviceDimensions(
                     end = 90.dp,
                     top = 72.dp,
                     bottom = 50.dp,
-                    normalTextStyle = TextStyle(
-                        fontSize = 32.sp,
-                        lineHeight = 24.sp,
-                        letterSpacing = 0.15.sp,
-                        fontFamily = FontFamily.Cursive
-                    ),
-
-                    boldTextStyle = TextStyle(
-                        fontSize = 32.sp,
-                        lineHeight = 24.sp,
-                        letterSpacing = 0.15.sp,
-                        fontFamily = FontFamily.SansSerif
-                    ),
                 )
             } else {
                 DeviceDimensions(
@@ -116,71 +60,23 @@ fun getDeviceDimensions(
                     end = 66.dp,
                     bottom = 200.dp,
                     top = 200.dp,
-                    normalTextStyle = TextStyle(
-                        fontSize = 32.sp,
-                        lineHeight = 24.sp,
-                        letterSpacing = 0.15.sp,
-                        fontFamily = FontFamily.Cursive
-                    ),
-
-                    boldTextStyle = TextStyle(
-                        fontSize = 32.sp,
-                        lineHeight = 24.sp,
-                        letterSpacing = 0.15.sp,
-                        fontFamily = FontFamily.SansSerif
-                    ),
                 )
             }
         }
     }
 }
 
-@SuppressLint("RestrictedApi")
-@Composable
-fun PreviewViewComposable() {
-    AndroidView({ context ->
-        val cameraExecutor = Executors.newSingleThreadExecutor()
-        val previewView = PreviewView(context).also {
-            it.scaleType = PreviewView.ScaleType.FILL_CENTER
-            it.background = ContextCompat.getDrawable(context, R.drawable.back)
-            it.implementationMode = PreviewView.ImplementationMode.COMPATIBLE
+fun getFontStyle(deviceType: DeviceType):TextStyle{
+    return when(deviceType){
+        DeviceType.MOBILE -> {
+            TextStyle(
+                fontSize = 16.sp,
+            )
         }
-        val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
-        cameraProviderFuture.addListener({
-            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
-
-            val preview = Preview.Builder()
-                .build()
-                .also {
-                    it.setSurfaceProvider(previewView.surfaceProvider)
-                }
-
-            val imageCapture = ImageCapture.Builder().build()
-
-            val imageAnalyzer = ImageAnalysis.Builder()
-                .build()
-                .also {
-                    it.setAnalyzer(cameraExecutor, BarcodeAnalyser{
-                        Toast.makeText(context, it, Toast.LENGTH_LONG).show()
-                    })
-                }
-
-            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-
-            try {
-                // Unbind use cases before rebinding
-                cameraProvider.unbindAll()
-
-                // Bind use cases to camera
-                cameraProvider.bindToLifecycle(
-                    context as ComponentActivity, cameraSelector, preview, imageCapture, imageAnalyzer)
-
-            } catch(exc: Exception) {
-                Log.e("DEBUG", "Use case binding failed", exc)
-            }
-        }, ContextCompat.getMainExecutor(context))
-        previewView
-    },
-        modifier = Modifier
-            .size(width = 250.dp, height = 250.dp))
+        DeviceType.TABLET -> {
+            TextStyle(
+                fontSize = 28.sp,
+            )
+        }
+    }
 }
